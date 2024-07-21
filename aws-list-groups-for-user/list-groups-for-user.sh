1d0adaab5d246fc056be4168ef8a637f09cd742d
RoniAngress-wavebl <roni.angress@wavebl.com>
2024-07-21 17:25:13 +0300

    added new aws-list-groups script

---
+++ aws-list-groups-for-user/list-groups-for-user.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# List all users
+USERS=$(aws iam list-users --query 'Users[*].UserName' --output text)
+
+for USER in $USERS; do
+  echo "Groups for user: $USER"
+  aws iam list-groups-for-user --user-name $USER --query 'Groups[*].GroupName' --output text > groups.txt
+  echo "------"
+done
