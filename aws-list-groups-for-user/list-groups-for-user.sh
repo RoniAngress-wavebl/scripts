7a523e801b6307a0b5d9e2d8126260b9b2c3dd0f
RoniAngress-wavebl <roni.angress@wavebl.com>
2024-07-22 15:09:59 +0300

    Modified to echo results at console

--- aws-list-groups-for-user/list-groups-for-user.sh
+++ aws-list-groups-for-user/list-groups-for-user.sh
@@ -5,6 +5,6 @@ USERS=$(aws iam list-users --query 'Users[*].UserName' --output text)
 
 for USER in $USERS; do
   echo "Groups for user: $USER"
-  aws iam list-groups-for-user --user-name $USER --query 'Groups[*].GroupName' --output text > groups.txt
+  aws iam list-groups-for-user --user-name $USER --query 'Groups[*].GroupName' --output text
   echo "------"
 done
