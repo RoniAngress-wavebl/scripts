bbc63ee4db98087be30817807f4c7dcf050e135a
RoniAngress-wavebl <roni.angress@wavebl.com>
2024-12-19 10:48:57 +0200

    modified script:entra_join

--- join_entra/entra_joined_detection.ps1
+++ join_entra/entra_joined_detection.ps1
@@ -5,15 +5,6 @@ $successFilePath = "$env:ProgramData\provisioning_success.txt"
 if (Test-Path $successFilePath) {
     Write-Host "Success file found at $successFilePath."
 
-    # Optional: Validate content or timestamp
-    $fileContent = Get-Content -Path $successFilePath
-    if ($fileContent -match "Provisioning package applied successfully") {
-        Write-Host "Success file content is valid. Provisioning was successful."
-        exit 0
-    } else {
-        Write-Host "Success file content is invalid. Provisioning may have failed."
-        exit 1
-    }
 } else {
     Write-Host "Success file not found. Provisioning may not have been applied."
     exit 1
