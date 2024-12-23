62cd2cab5623c9daac9be6939a92d10688b236b0
RoniAngress-wavebl <roni.angress@wavebl.com>
2024-12-23 12:20:21 +0200

    Updated: join_entra script logic

--- join_entra/entra_joined_detection.ps1
+++ join_entra/entra_joined_detection.ps1
@@ -1,9 +1,11 @@
 # Variables
-$successFilePath = "$env:ProgramData\provisioning_success.txt"
+$scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Path
+$localPath = Join-Path $scriptDirectory "entra_join.ppkg"
 
 # Check if the provisioning success file exists
-if (Test-Path $successFilePath) {
-    Write-Host "Success file found at $successFilePath."
+if (Test-Path $localPath) {
+    Write-Host "Success file found at $localPath."
+    exit 0
 
 } else {
     Write-Host "Success file not found. Provisioning may not have been applied."
